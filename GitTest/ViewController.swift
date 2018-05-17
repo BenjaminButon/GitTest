9f0a06eb96cd1376863e7fbe85ba55d14145352e
BenjaminButon <Buratino1324>
2018-05-17 11:18:52 +0300

    Add array of persons

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var txt: UITextField!
     @IBOutlet weak var btn: UIButton!
     
+    var persons = [Person]()
     override func viewDidLoad() {
         super.viewDidLoad()
         
