fe84ac9767900304de5d9b1d415c1ce81edd4be1
BenjaminButon <Buratino1324>
2018-05-17 11:09:06 +0300

    Add person class

---
+++ GitTest/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  GitTest
+//
+//  Created by Benko Ostap on 17.05.18.
+//  Copyright © 2018 Ostap Benko. All rights reserved.
+//
+
+import Foundation
+
+class Person{
+    var name = ""
+    var surname = ""
+    var age  = 0
+}
